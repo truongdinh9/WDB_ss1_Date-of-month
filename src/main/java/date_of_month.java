1a5c0aa3ba80abb45ae4e188ed32e543558eb4f2
dinh <dinh.kttn.neu@gmail.com>
2019-03-26 21:22:06 +0700

    commit file java

---
+++ src/main/java/date_of_month.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class date_of_month {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap thang: ");
+        int month = scanner.nextInt();
+        switch (month) {
+            case 2:
+                System.out.println("Thang 2 co 28 hoac 29 ngay.");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Thang " + month + "co 31 ngay.");
+                break;
+            default:
+               if(month<12) {System.out.println("Thang " + month + "co 30 ngay.");}
+        }
+    }
+}
